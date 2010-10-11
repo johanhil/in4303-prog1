f3ed8135002b77b8f675a4178e570d0e78397eec
unknown <Vlad@.(none)>
2010-10-11 11:31:33 +0200

    I added the DynamicProgramming class(incomplete)

---
+++ DynamicProgramming.java
@@ -0,0 +1,147 @@
+package programming1;
+
+public class DynamicProgramming {
+	
+	private JobList 	jobs;
+	private int[][][][]	Tardiness;
+	/**
+	 * The constructor assumes it receives jobs in non-decreasing deadline order
+	 * @param j The list of jobs in non-decreasing deadline order
+	 * */
+	public DynamicProgramming(JobList jList)
+	{
+		jobs 		= jList;
+		int n 		= jobs.size();
+		Tardiness 	= new int[n][n][n][n*maxProcessingTime()];
+	}
+	
+	/**
+	 * @return The minimum total tardiness as computed by the Dynamic Programming algorithm 
+	 * */
+	public int minimumTotalTardiness()
+	{
+		return tardiness(0,jobs.size()-1,maxProcessingTimeIndex(),0);
+	}
+	
+	private int tardiness(int i, int j, int k, int t)
+	{
+		/*
+		 * TODO: This method still needs to be implemented to work correctly and faster
+		 * The other helper methods are hopefully ok and useful
+		 */
+		if( i > j ) //Empty set
+			return 0;
+		if(Tardiness[i][j][k][t] != 0)	//already computed
+			return Tardiness[i][j][k][t];
+		int minTardiness = Integer.MAX_VALUE;
+		for(int delta = 0; delta < jobs.size() - k; delta++)
+		{
+			int kPrime = maxProcessingTimeIndex(i,j,k);
+			if (kPrime == -1)//S(i,j,k) is empty
+				return 0;
+			if(sum(i,k+delta,kPrime) == -1)// empty set
+				return 0;
+			Tardiness[i][j][k][t] = tardiness(i,k+delta,kPrime,t) + Math.max(0,t + sum(i,k+delta,kPrime) - jobs.get(kPrime).getDue()) +
+			   						tardiness(kPrime + delta + 1, j, kPrime, sum(i,k+delta,kPrime));
+			if(Tardiness[i][j][k][t] < minTardiness)
+				minTardiness = Tardiness[i][j][k][t];
+		}
+		return minTardiness;
+	}
+
+	/**
+	 * @return The maximum processing time of all jobs, or -1 if the list is null or empty
+	 * */
+	public int maxProcessingTime()
+	{
+		if(jobs == null)
+			return -1;
+		if(jobs.size() == 0 )
+			return -1;
+		JobList.MaxProcessingTimeComparator comparator = new JobList.MaxProcessingTimeComparator();
+		Job maximumJob = jobs.get(0); 
+		for (Job j : this.jobs) {
+			if(comparator.compare(j, maximumJob) > 0)
+				maximumJob = j;
+		}
+		return maximumJob.getLength();
+	}
+	
+	/**
+	 * @return The sum of processing times of all jobs within S(i,j,k), or 0 if the set is empty
+	 * */
+	public int sum(int i,int j,int k)
+	{
+		if(jobs == null)
+			return 0;
+		if(jobs.size() == 0 )
+			return 0;
+		JobList.MaxProcessingTimeComparator comparator = new JobList.MaxProcessingTimeComparator();
+		int sum = 0;
+		try	{
+			Job jobK = jobs.get(k);
+			for (Job job : this.jobs.subList(i, j+1)) {
+				if(comparator.compare(job, jobK) < 0)
+					sum += job.getLength();
+			}
+		}catch(Exception ex)
+		{
+			sum = 0;
+		}
+		return sum;
+	}
+	
+	/**
+	 * @return The index of the job having the maximum processing time, or -1 if the list is null or empty
+	 * */
+	public int maxProcessingTimeIndex()
+	{
+		if(jobs == null)
+			return -1;
+		if(jobs.size() == 0 )
+			return -1;
+		JobList.MaxProcessingTimeComparator comparator = new JobList.MaxProcessingTimeComparator();
+		Job maximumJob 	= jobs.get(0);
+		int index 		= 0;
+		int maxIndex 	= 0; 
+		for (Job j : this.jobs) {
+			if(comparator.compare(j, maximumJob) > 0)
+			{
+				maximumJob 	= j;
+				maxIndex 	= index;
+			}
+			index++;
+		}
+		return maxIndex;
+	}
+	
+	/**
+	 * @return The index of the job between job i and job j (including i and j) having the 
+	 * maximum processing time strictly less than the processing time of job k, or -1 if the 
+	 * list is null or empty or if there is no such job. 
+	 * */
+	public int maxProcessingTimeIndex(int i,int j,int k)
+	{
+		if(jobs == null || i > j)
+			return -1;
+		JobList.MaxProcessingTimeComparator comparator = new JobList.MaxProcessingTimeComparator();
+		int maxIndex	= -1;
+		try{
+			Job jobK 		= jobs.get(k);
+			Job maximumJob	= new Job(-1,-1);
+			int index		= i;
+			for (Job job : this.jobs.subList(i, j+1)) {
+				if((comparator.compare(job, maximumJob)) > 0 && (comparator.compare(job, jobK) < 0))
+				{
+					maximumJob 	= job;
+					maxIndex 	= index;
+				}
+				index++;
+			}
+		}catch(Exception e)
+		{
+			maxIndex = -1;
+		}
+		return maxIndex;
+	}
+}
---
+++ DynamicProgrammingTest.java
@@ -0,0 +1,77 @@
+package programming1;
+
+import java.io.*;
+import java.util.Collections;
+import java.util.Scanner;
+
+import org.junit.After;
+import org.junit.Before;
+
+import junit.framework.*;
+
+public class DynamicProgrammingTest extends TestCase{
+	
+	private DynamicProgramming DP;
+	
+	public DynamicProgrammingTest(String name)
+	{
+		super(name)	;
+		DP 	= null	;
+	}
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		Scanner s = null;
+		int numJobs = 0;
+		JobList jobs = new JobList();
+		
+		try {
+			s = new Scanner(new BufferedReader(new FileReader("tests/random_RDD=0.2_TF=0.2_#25.dat")));
+			
+			if (s.hasNextInt()) {
+				numJobs = s.nextInt();
+			}
+			
+			for (int i = 0; i < numJobs; i++) {
+				jobs.add(new Job(s.nextInt(), s.nextInt()));
+			}
+			s.close();
+		} catch (Exception e) {
+			System.err.println(e);
+		}
+		Collections.sort(jobs, new JobList.NonDecreasingDeadlineOrderComparator());
+		DP = new DynamicProgramming(jobs);
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+	public void runTest()
+	{
+		testMaximumProcessingTime();
+	}
+	
+	public void testMaximumProcessingTime()
+	{
+		assertEquals(DP.maxProcessingTime		(),			93);
+		assertEquals(DP.maxProcessingTimeIndex	(),			14);
+		assertEquals(DP.maxProcessingTimeIndex	(0,2,1), 	 0);
+		assertEquals(DP.maxProcessingTimeIndex	(4,9,4), 	 7);
+		assertEquals(DP.maxProcessingTimeIndex	(4,4,4),	-1);
+		assertEquals(DP.maxProcessingTimeIndex	(1,5,5),	-1);
+		assertEquals(DP.maxProcessingTimeIndex	(1,5,4), 	 3);
+		assertEquals(DP.maxProcessingTimeIndex	(1,5,14),	 4);
+		assertEquals(DP.maxProcessingTimeIndex	(-1,1,1),	-1);
+		assertEquals(DP.sum						(-1,1,1),	 0);
+		assertEquals(DP.sum						(0,3,3),   193);
+		assertEquals(DP.sum						(1,1,1),   	 0);
+		assertEquals(DP.sum						(1,1,2),     0);
+	}
+}
